a2fcb3d0d4823ac1cc2dcd001be62895b4026caa
wegegen <wegeneargow11@gmail.com>
2023-07-01 08:56:25 +0300

    many change on the login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import logo from './images/logo.svg';
 import LoginValidation from './LoginValidation';
 
 
